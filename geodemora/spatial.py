5baf8523ccd406ccba7d75c596eaf7f799492735
Ramendra Sahoo <ramendrageo@gmail.com>
2022-06-05 17:55:20 +0530

    Added some gdal utilities

--- geodemora/spatial.py
+++ geodemora/spatial.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+from osgeo import gdal
 
 def nFields(shapeFile):
     """Returns number of fields in a shapefile
@@ -6,3 +7,14 @@ def nFields(shapeFile):
 
     gdf = gpd.read_file(shapeFile)
     return len(gdf.columns)
+
+def rasterInfo(raster):
+    
+        ds = gdal.Open(raster)
+        xRes = ds.GetGeoTransform()[1]
+        yRes = ds.GetGeoTransform()[-1] * (-1)
+        ncols = ds.RasterXSize
+        nrows = ds.RasterYSize
+        
+        return {'ncols' : ncols, 'nrows' : nrows, 
+                'xRes' : xRes, 'yRes' : yRes}
