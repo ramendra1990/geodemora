be1b05dc75030d15e21e7973f015d27be68e021c
Ramendra Sahoo <ramendrageo@gmail.com>
2022-06-05 19:27:01 +0530

    again removed gdal

--- geodemora/spatial.py
+++ geodemora/spatial.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
-from osgeo import gdal
+# from osgeo import gdal
 
 def nFields(shapeFile):
     """Returns number of fields in a shapefile
@@ -8,13 +8,13 @@ def nFields(shapeFile):
     gdf = gpd.read_file(shapeFile)
     return len(gdf.columns)
 
-def rasterInfo(raster):
+# def rasterInfo(raster):
     
-        ds = gdal.Open(raster)
-        xRes = ds.GetGeoTransform()[1]
-        yRes = ds.GetGeoTransform()[-1] * (-1)
-        ncols = ds.RasterXSize
-        nrows = ds.RasterYSize
+#         ds = gdal.Open(raster)
+#         xRes = ds.GetGeoTransform()[1]
+#         yRes = ds.GetGeoTransform()[-1] * (-1)
+#         ncols = ds.RasterXSize
+#         nrows = ds.RasterYSize
         
-        return {'ncols' : ncols, 'nrows' : nrows, 
-                'xRes' : xRes, 'yRes' : yRes}
+#         return {'ncols' : ncols, 'nrows' : nrows, 
+#                 'xRes' : xRes, 'yRes' : yRes}
